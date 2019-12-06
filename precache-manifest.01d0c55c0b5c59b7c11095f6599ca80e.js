53c2a78d14628e0bd7e0262da63b579948ce4584
Spencer Dedrick <spencer.dedrick@gmail.com>
2019-12-06 11:15:18 -0600

    Robofriends first commit

---
+++ precache-manifest.01d0c55c0b5c59b7c11095f6599ca80e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "51ef44075b064ccb5a4e81f8b7edb51e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e97070d393ad87044474",
+    "url": "/static/css/2.eb45ad44.chunk.css"
+  },
+  {
+    "revision": "d845bfe9f952d622f992",
+    "url": "/static/css/main.034fefe5.chunk.css"
+  },
+  {
+    "revision": "e97070d393ad87044474",
+    "url": "/static/js/2.814c5b91.chunk.js"
+  },
+  {
+    "revision": "d845bfe9f952d622f992",
+    "url": "/static/js/main.4920ecd3.chunk.js"
+  },
+  {
+    "revision": "9bf820bd41e6d656f8db",
+    "url": "/static/js/runtime-main.f59df13c.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/static/media/SEGA.12a3db8b.woff"
+  }
+]);
